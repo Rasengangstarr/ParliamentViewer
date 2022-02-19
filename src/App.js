57e17025facc5aa5b3f9ca45fff7f55f38f80241
Rasengangstarr <raseng@protonmail.com>
2022-02-19 13:10:59 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MemberCard from "./components/MemberCard";
+
+function App() {
+  return (
+    <div>
+      <h1>Members of Parliament</h1>
+      <MemberCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MemberCard.js
@@ -0,0 +1,12 @@
+function MemberCard() {
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className="card-actions">
+        <button className="button">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default MemberCard;
